f5b485202a4b751419f9ab28c65136fa9a1c55c1
Muttaqi <mohammutt@sheridancollege.ca>
2025-02-27 03:15:36 -0500

    created start method

--- game.py
+++ game.py
@@ -1,5 +1,11 @@
 import random
 class Game:
-    def __init__(self,players,guess)
+    def __init__(self,players,guess):
         self.players = ["player1","player2","player3"]
         self.guess = None 
+
+    def start(self):
+        self.guess = random.randint(1,6)
+    
+    def match(self):
+        
