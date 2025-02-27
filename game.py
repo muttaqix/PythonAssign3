80873a7589068140deb1415fa3eb512c90199485
Muttaqi <mohammutt@sheridancollege.ca>
2025-02-27 02:53:28 -0500

    updated players and guess attributes

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
 import random
-players = ["player1","player2","player3"]
-guess = random.randint(1,6)
+class Game:
+    def __init__(self,players,guess)
+        self.players = ["player1","player2","player3"]
+        self.guess = None 
