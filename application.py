d8825a1312214f9a4ca0d020140251a552326761
Muttaqi <mohammutt@sheridancollege.ca>
2025-02-27 06:13:42 -0500

    created method called match and instance of game

--- application.py
+++ application.py
@@ -1,2 +1,6 @@
 from game import Game
 class Application:
+    def __init__(self):
+        self.game = Game()
+        
+
--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import random
+from player import Player
 class Game:
     def __init__(self,players,guess):
         self.players = ["player1","player2","player3"]
@@ -8,4 +9,8 @@ class Game:
         self.guess = random.randint(1,6)
     
     def match(self):
-        
+        for i in self.players:
+            if self.roll == self.guess:
+                self.score +=1
+                
+
