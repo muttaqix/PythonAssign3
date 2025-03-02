557b490800395da0fe9df1f09a752d8b9658e4a8
Muttaqi <mohammutt@sheridancollege.ca>
2025-03-02 07:04:34 -0500

    created attributes for account

---
+++ account.py
@@ -0,0 +1,8 @@
+class Account:
+    def __init__(self,acct_type,min_balance,current_balance):
+        self.acct_type = acct_type
+        self.min_balance = 500
+        self.current_balance = []
+    
+    
+        
